bc8ceda685537bf21cd786fc40f5dcd3c5788797
Matt Kinne <mattkinne@gmail.com>
2024-09-24 11:55:36 -0500

    Finishing tutorial, completing login, error handling, and more

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -5,6 +5,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
  
 export default async function Page() {
 
