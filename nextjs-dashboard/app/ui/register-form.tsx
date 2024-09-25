9103b9082a7a1111c6c4239c047b4f01a6b8fd09
Matt Kinne <mattkinne@gmail.com>
2024-09-25 16:27:48 -0500

    Fixing issue with name form on registration

--- nextjs-dashboard/app/ui/register-form.tsx
+++ nextjs-dashboard/app/ui/register-form.tsx
@@ -32,7 +32,7 @@ export default function RegisterForm() {
                     <div className="mt-4">
                         <label
                         className="mb-3 mt-5 block text-xs font-medium text-gray-900 dark:text-white"
-                        htmlFor="email"
+                        htmlFor="name"
                         >
                         Name
                         </label>
@@ -40,12 +40,12 @@ export default function RegisterForm() {
                         <input
                             className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500 dark:bg-slate-800 dark:text-white"
                             id="name"
-                            type="name"
+                            type="text"
                             name="name"
                             placeholder="Enter your name"
                             required
                         />
-                        <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900 dark:text-white" />
+                        <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900 dark:text-white" />
                         </div>
                     </div>
                     <div className="mt-4">
