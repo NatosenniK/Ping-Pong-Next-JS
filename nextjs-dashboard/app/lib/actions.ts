234caa7709a82f624cfa061ede561e9048d32c82
Matt Kinne <mattkinne@gmail.com>
2024-10-15 16:12:49 -0500

    Removing edit invoice leftovers from template

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -106,45 +106,6 @@ export async function createMatch(prevState: State, formData: FormData): Promise
   redirect('/dashboard/matches');
 }
 
-  
-
-// const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-// export async function updateInvoice(
-//     id: string,
-//     prevState: State,
-//     formData: FormData,
-//   ) {
-//     const validatedFields = UpdateInvoice.safeParse({
-//       customerId: formData.get('customerId'),
-//       amount: formData.get('amount'),
-//       status: formData.get('status'),
-//     });
-   
-//     if (!validatedFields.success) {
-//       return {
-//         errors: validatedFields.error.flatten().fieldErrors,
-//         message: 'Missing Fields. Failed to Update Invoice.',
-//       };
-//     }
-   
-//     const { customerId, amount, status } = validatedFields.data;
-//     const amountInCents = amount * 100;
-   
-//     try {
-//       await sql`
-//         UPDATE invoices
-//         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-//         WHERE id = ${id}
-//       `;
-//     } catch (error) {
-//       return { message: 'Database Error: Failed to Update Invoice.' };
-//     }
-   
-//     revalidatePath('/dashboard/invoices');
-//     redirect('/dashboard/invoices');
-// }
-
 export async function deleteMatch(id: string) {
 
     try {
