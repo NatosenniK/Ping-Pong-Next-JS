b52400e258cab521a2a6d40ae79f84234f838f12
Matt Kinne <mattkinne@gmail.com>
2024-10-16 15:31:01 -0500

    Troubleshooting AWS on prod

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -3,7 +3,11 @@ import SideNav from '@/app/ui/dashboard/sidenav';
 export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
-
+  console.log("Bucket Name:", process.env.AWS_ACCESS_KEY_ID)
+  console.log("Bucket Name:", process.env.AWS_SECRET_ACCESS_KEY)
+  console.log("Bucket Name:", process.env.AWS_REGION)
+  console.log("Bucket Name:", process.env.AWS_BUCKET_NAME)
+  console.log("Bucket Name:", process.env.NEXT_PUBLIC_BASE_URL)
   return (
     <div className="bg-white dark:bg-slate-800 flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
